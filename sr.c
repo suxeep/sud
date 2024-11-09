e6bc09561e5dff1ba91558aede2292cce18c622b
suxeep <rsudeep660@gmail.com>
2024-11-09 10:12:36 +0530

    commit 10

--- sr.c
+++ sr.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 void main
 {
-printf("commit 1");
-printf("commit 2");
-printf("commit 3");
-printf("commit 4");
-printf("commit 5");
-printf("commit 6");
-printf("commit 7");
-printf("commit 8");
-printf("commit 9");
+printf("commit 1 from fb");
+printf("commit 2 from fb");
+printf("commit 3 from fb");
+printf("commit 4 from fb");
+printf("commit 5 frpm fb");
+printf("commit 6 from master");
+printf("commit 7 from master");
+printf("commit 8 from master");
+printf("commit 9 from master");
+printf("commit 10 from master");
 }
